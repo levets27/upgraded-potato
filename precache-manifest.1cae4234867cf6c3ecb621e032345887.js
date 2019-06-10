7b8fb849575b5743b7353c09bf16a725b8d649f9
Steve Leach <sleach@wayfair.com>
2019-06-10 15:30:09 -0400

    Updates

---
+++ precache-manifest.1cae4234867cf6c3ecb621e032345887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac844eefb20faa6ac5f6699cccd28ac0",
+    "url": "/upgraded-potato/index.html"
+  },
+  {
+    "revision": "b0c01ff72386c6af8e6b",
+    "url": "/upgraded-potato/static/css/main.86a2c362.chunk.css"
+  },
+  {
+    "revision": "4186d027ea0ef0128a1a",
+    "url": "/upgraded-potato/static/js/2.83d0676a.chunk.js"
+  },
+  {
+    "revision": "b0c01ff72386c6af8e6b",
+    "url": "/upgraded-potato/static/js/main.033d1683.chunk.js"
+  },
+  {
+    "revision": "59497d7c366bb0c2aadc",
+    "url": "/upgraded-potato/static/js/runtime~main.6aa2da0d.js"
+  }
+]);
